3a60958e35ef7f38e9f40075a538643af04944be
Xomon Xihau <57447181+xomon-xihau@users.noreply.github.com>
2019-11-10 02:45:33 +0530

    Update atg_notification.fish

--- atg_notification.fish
+++ atg_notification.fish
@@ -1,11 +1,19 @@
 #!/usr/bin/env fish
+set FILE $HOME/.oldchap
+if not test -f $FILE
+    touch $HOME/.oldchap
+end
+if not type --quiet 'curl'
+    echo 'Plz install curl'
+    exit 1
+end
 while true
-    set OLDCHAP (cat $HOME/.oldchap)
+    set OLDCHAP (cat $FILE)
     set NEWCHAP (curl -s 'http://book.zongheng.com/book/408586.html' | grep -oP '第.*章.*(?<=/a)')
     if test $NEWCHAP != $OLDCHAP
         notify-send 'Raw is out'
         notify-send $NEWCHAP
-        echo $NEWCHAP > $HOME/.oldchap
+        echo $NEWCHAP > $FILE
     end
     sleep 100
 end
